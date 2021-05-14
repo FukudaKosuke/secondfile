afa650c4d6d7ad4a38d1402afd61422ed5ffc2d1
FukudaKosuke <70466093+FukudaKosuke@users.noreply.github.com>
2021-05-14 21:55:31 +0900

    initial commit.
    練習中。

---
+++ 基本文法＜1＞.js
@@ -0,0 +1,24 @@
+let 価格=100;
+let 売値=価格*1.08;
+console.log(売値);
+let カカク=100;
+let ウリネ=カカク*1.08;
+console.log(ウリネ);
+let 文字 =prompt("入力せよ");
+console.log(文字);
+let もじ =prompt("入力せよ");
+console.log("入力したのは"+もじ);
+let かかく =prompt("価格を入力せよ");
+console.log(かかく+80);
+console.log("ハロー！",10,3.5);
+let モジ=prompt("入力せよ");
+console.log(isNaN(モジ));
+let 文字Ⅰ=prompt("入力せよ");
+if(isNaN(文字Ⅰ)){
+ console.log("数字ではない");
+}
+let moji=prompt("入力せよ");
+if(!isNaN(moji)){
+ console.log(parseInt(moji)+80);
+}
+
